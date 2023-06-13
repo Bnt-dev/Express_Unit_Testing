5a70eaf3d2387dd3db8370322c5ab1bb8a4061ae
Bnt-dev <bberanger.pro@gmail.com>
2023-06-13 15:38:48 +0200

    Routes get albums

--- src/routes/albums/albumsController.js
+++ src/routes/albums/albumsController.js
@@ -2,41 +2,31 @@ const connexion = require('../../../db-config');
 const db = connexion.promise();
 
 const getAll = (req, res) => {
-  db
-    .query("SELECT * FROM albums")
+  db.query('SELECT * FROM albums')
     .then(([results]) => res.json(results))
-    .catch((err) => res.status(500).send('Error retrieving data from database'))
+    .catch(() => res.status(500).send('Error retrieving data from database'));
 };
-
-
-
-
 
 const getOne = (req, res) => {
-  const id = parseInt(req.params.id)
+  const id = parseInt(req.params.id);
 
-  db
-    .query('SELECT * FROM albums WHERE id = ?', [id])
-    .then(([results]) => {
-      if (results.length) {
-        res.json(results)
+  db.query('SELECT * FROM albums WHERE id = ?', [id])
+    .then(([[results]]) => {
+      if (results) {
+        res.json(results);
+      } else {
+        res.status(404).send('Not found');
       }
-      else { res.status(404).send('Not found') }
     })
-    .catch((err) => res.status(500).send('Error retrieving data from database'))
+    .catch(() => res.status(500).send('Error retrieving data from database'));
 };
 
-
-
-
-
 const getTracksByAlbumId = (req, res) => {
-  const id = parseInt(req.params.id)
+  const id_album = parseInt(req.params.id);
 
-  db
-    .query('SELECT title FROM tracks WHERE id = ?', [id])
+  db.query('SELECT * FROM track WHERE id_album = ?', [id_album])
     .then(([results]) => res.json(results))
-    .catch((err) => res.send(404).send('found'))
+    .catch(() => res.status(404).send('Not found'));
 };
 
 const postAlbums = (req, res) => {
